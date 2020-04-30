9c4aa336596ca7d40b416640d4596711e3a095b1
ak98neon <ak98nneon@gmail.com>
2020-04-30 16:06:53 +0300

    Minor setup changes

--- src/main/java/com/ak98neon/todo/TodoApplication.java
+++ src/main/java/com/ak98neon/todo/TodoApplication.java
@@ -1,5 +1,6 @@
 package com.ak98neon.todo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodoApplication.class, args);
+        SpringApplication app = new SpringApplication(TodoApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
 }
